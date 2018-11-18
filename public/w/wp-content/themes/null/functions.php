d2369a25fb0eae784d4d16be422bd02f7ce42e6b
weboloper <weboloper@gmail.com>
2018-11-18 20:34:51 +0300

    select2 and function ready

--- public/w/wp-content/themes/null/functions.php
+++ public/w/wp-content/themes/null/functions.php
@@ -6,6 +6,8 @@
 // }
 // add_shortcode( 'shortcode', 'shortcode_function' );
 
+add_action('after_setup_theme', 'after_setup_theme_example');
+
 if (! function_exists('after_setup_theme_example')) :
 
 	function after_setup_theme_example() {
@@ -38,31 +40,68 @@ if (! function_exists('after_setup_theme_example')) :
 	}
 endif;
 
-add_action('after_setup_theme', 'after_setup_theme_example');
+
+// function enqueue_my_scripts() {
+
+//     // wp_enqueue_style( 'theme-style', get_stylesheet_uri(), array( 'reset' ) );
+// 	// wp_enqueue_style( 'reset', get_stylesheet_directory_uri() . '/reset.css' );
+
+// 	// wp_enqueue_script( 'owl-carousel', get_stylesheet_directory_uri() . '/owl.carousel.js', array( 'jquery' ) );
+// 	// wp_enqueue_script( 'theme-scripts', get_stylesheet_directory_uri() . '/website-scripts.js', array( 'owl-carousel', 'jquery' ), '1.0', true );
+
+// 	wp_enqueue_style( 'select2-css', get_stylesheet_directory_uri() . '/assets/select2/select2.min.css' , array( 'select2-css' ) );
+// 	wp_enqueue_script( 'select2-js', get_stylesheet_directory_uri() . '/assets/select2/select2.min.js', array( 'select2-js', 'jquery' ), '1.0', true );
+
+// }
+// add_action( 'admin_enqueue_scripts', 'enqueue_my_scripts' );
 
 
+
+// LOADIN ADMIN STYLES
+function load_custom_wp_admin_style() {
+        wp_register_style( 'selec2_css', get_template_directory_uri() . '/assets/select2/select2.min.css', false, '1.0.0' );
+        wp_enqueue_style( 'selec2_css' );
+
+        wp_enqueue_script( 'selec2_js',  get_template_directory_uri() . '/assets/select2/select2.min.js' , array(), '1.0' );
+}
+add_action( 'admin_enqueue_scripts', 'load_custom_wp_admin_style' );
+    
+
+function admin_inline_js(){
+	echo "<script type='text/javascript'>\n";
+	echo " $('.select2-select').select2()";
+	echo "\n</script>";
+}
+add_action( 'admin_print_footer_scripts', 'admin_inline_js' );
+
+
+
+// https://wordpress.stackexchange.com/questions/128622/how-do-i-create-a-relationship-between-two-custom-post-types
 add_action( 'admin_init', 'add_meta_boxes' );
 function add_meta_boxes() {
-    add_meta_box( 'some_metabox', 'Movies Relationship', 'post', 'posts' );
+    add_meta_box( 'multiple_metabox', 'Multiple field relationship', 'multiple_field', 'post' );
 }
 
-function movies_field() {
+function multiple_field() {
     global $post;
-    $selected_movies = get_post_meta( $post->ID, '_movies', true );
-    $all_movies = get_posts( array(
-        'post_type' => 'movies',
+
+    $selected_relations = get_post_meta( $post->ID, '_field_meta_key', true );
+    $selected_relations = is_array($selected_relations) ? $selected_relations : [] ;
+
+    $all_options = get_posts( array(
+        'post_type' => 'post',
         'numberposts' => -1,
         'orderby' => 'post_title',
         'order' => 'ASC'
     ) );
     ?>
-    <input type="hidden" name="movies_nonce" value="<?php echo wp_create_nonce( basename( __FILE__ ) ); ?>" />
+    <input type="hidden" name="verify_key" value="<?php echo wp_create_nonce( basename( __FILE__ ) ); ?>" />
     <table class="form-table">
     <tr valign="top"><th scope="row">
-    <label for="movies">Movies</label></th>
-    <td><select multiple name="movies">
-    <?php foreach ( $all_movies as $movie ) : ?>
-        <option value="<?php echo $movie->ID; ?>"<?php echo (in_array( $movie->ID, $selected_movies )) ? ' selected="selected"' : ''; ?>><?php echo $movie->post_title; ?></option>
+    <label>Multiple field relationship Label</label></th>
+    <td><select multiple name="select_value[]" class="select2-select" style="width:100%">
+    <?php foreach ( $all_options as $single_option ) : ?>
+        <option value="<?php echo $single_option->ID; ?>"<?php echo (in_array( $single_option->ID, $selected_relations )) ? ' selected="selected"' : ''; ?>><?php echo $single_option->post_title; ?></option>
     <?php endforeach; ?>
     </select></td></tr>
     </table>
@@ -70,16 +109,16 @@ function movies_field() {
     <?php 
 }
 
-add_action( 'save_post', 'save_movie_field' );
+add_action( 'save_post', 'save_multiple_field' );
 
-function save_movie_field( $post_id ) {
+function save_multiple_field( $post_id ) {
 
-    // only run this for series
-    if ( 'series' != get_post_type( $post_id ) )
+    // check post type
+    if ( 'post' != get_post_type( $post_id ) )
         return $post_id;        
 
     // verify nonce
-    if ( empty( $_POST['movies_nonce'] ) || !wp_verify_nonce( $_POST['movies_nonce'], basename( __FILE__ ) ) )
+    if ( empty( $_POST['verify_key'] ) || !wp_verify_nonce( $_POST['verify_key'], basename( __FILE__ ) ) )
         return $post_id;
 
     // check autosave
@@ -91,7 +130,7 @@ function save_movie_field( $post_id ) {
         return $post_id;
 
     // save
-    update_post_meta( $post_id, '_movies', array_map( 'intval', $_POST['movies'] ) );
+    update_post_meta( $post_id, '_field_meta_key', array_map( 'intval', $_POST['select_value'] ) );
 
 }
 
